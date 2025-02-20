bbdbe5dfc9f6fe1d6f0dc5fa01b0c564de69b638
avishkar.gawande <avishkargawande241@gmail.com>
2025-02-20 16:01:15 +0530

    react hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-
 function App() {
   const [number, setNumber] = useState("");
+  function checkNumber(event) {
+    setNumber(event.target.value)
+  }
   function EvenOdd() {
     if (number % 2 == 0) {
-      setNumber("number is even");
+      setNumber(number+" :number is even");
 
     }
     else {
-      setNumber("number is odd");
+      setNumber(number+" :number is odd");
 
     }
   }
-  return (
-    <div>
-      <h1>{number}</h1>
-      <input type="text" onChange={(e)=>{setNumber(e.target.value)}}></input>
-      <button onClick={EvenOdd}>submit</button>
-    </div>
-  );
+
+return (
+  <div>
+    <input type="text" onChange={checkNumber}></input>
+    <button onClick={EvenOdd}>submit</button>
+    <h1>{number}</h1>
+  </div>
+);
 }
 export default App;
