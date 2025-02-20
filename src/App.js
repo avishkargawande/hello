c8835c6591dbb92d38efaa00f84d9ae09c110abf
avishkar.gawande <avishkargawande241@gmail.com>
2025-02-20 14:54:43 +0530

    react hello

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const[name,setName]=useState("Hello world");
-  function changemsg(){
-   setName("Good Morning");
+  const [number, setNumber] = useState("");
+  function EvenOdd() {
+    if (number % 2 == 0) {
+      setNumber("number is even");
+
+    }
+    else {
+      setNumber("number is odd");
+
+    }
   }
   return (
-   <div>
-    <h1>{name}</h1>
-    <button onClick={changemsg}>change msg</button>
-   </div>
+    <div>
+      <h1>{number}</h1>
+      <input type="text" onChange={(e)=>{setNumber(e.target.value)}}></input>
+      <button onClick={EvenOdd}>submit</button>
+    </div>
   );
 }
-
 export default App;
