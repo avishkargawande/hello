51b7f3e89e0521cdf8619de29a4cf0dae443eace
avishkar.gawande <avishkargawande241@gmail.com>
2025-02-20 12:24:58 +0530

    react hello

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[name,setName]=useState("Hello world");
+  function changemsg(){
+   setName("Good Morning");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <h1>{name}</h1>
+    <button onClick={changemsg}>change msg</button>
+   </div>
   );
 }
 
